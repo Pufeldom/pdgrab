fb762fc01690edc2d769230d76d2cc6c2e74e2a3
Evgeni Dmitriev <edmitriev@ivi.ru>
2021-03-20 12:39:15 +0300

    Prefix grab sections in ini file

--- pdgrab/grab.py
+++ pdgrab/grab.py
@@ -12,11 +12,12 @@ def get_items():
     sources = config.get_param('grab', 'sources', True)
     for source_name in sources:
         source_module = import_module(__name__ + '_' + source_name)  # provide 'pdgrab.grab_<SOURCE_NAME>'
-        source_base_url = config.get_param(source_name, 'base_url')
-        source_section_prefix = config.get_param(source_name, 'section_prefix')
-        source_sections = config.get_param(source_name, 'sections', True)
+        source_config_section = 'grab' + '_' + source_name
+        source_base_url = config.get_param(source_config_section, 'base_url')
+        source_section_prefix = config.get_param(source_config_section, 'section_prefix')
+        source_sections = config.get_param(source_config_section, 'sections', True)
         try:
-            source_skipped_section_titles = config.get_param(source_name, 'skipped_section_titles', True)
+            source_skipped_section_titles = config.get_param(source_config_section, 'skipped_section_titles', True)
         except PdgrabConfigNoParamException:
             source_skipped_section_titles = []
         items = items + source_module.get_items(
